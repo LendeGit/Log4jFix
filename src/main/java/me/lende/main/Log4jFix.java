5643e9f7a8c66640a008eae88cd27003d57d86c5
Lenni <76701279+LendeGit@users.noreply.github.com>
2022-01-22 19:28:40 +0200

    Update Log4jFix.java

--- src/main/java/me/lende/main/Log4jFix.java
+++ src/main/java/me/lende/main/Log4jFix.java
@@ -13,6 +13,7 @@ public class Log4jFix implements Listener {
         if (event.getMessage().contains("jndi")||event.getMessage().contains("ldap")||event.getMessage().contains("{")||event.getMessage().contains("}")) {
             event.setCancelled(true);
             System.out.println(event.getPlayer().getName() + " tried to use Log4j exploit. Their message have been blocked from chat!");
+            player.sendMessage("§cThe Log4j exploit is already patched on this server!");
         }
     }
 }
